ee44366294335ad9204cc0455c3a60f7ab319746
Osama Asender <103122672+OsamaAsender@users.noreply.github.com>
2024-05-21 11:12:29 +0300

    added Angular Material package and added some entities
    -Added angular material typography
    - Added Category entity
    - Added Order entity
    - Added Product entity

---
+++ asp-net/OA.E-Cafe.Entities/Categories/Category.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OA.E_Cafe.Entities.Categories
+{
+    public class Category
+    {
+    }
+}
---
+++ asp-net/OA.E-Cafe.Entities/Orders/Order.cs
@@ -0,0 +1,15 @@
+﻿using OA.E_Cafe.Entities.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OA.E_Cafe.Entities.Orders
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
