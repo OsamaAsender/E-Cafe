54cc51c902a2253c478f287dd2ad16a6112c81c1
Osama Asender <103122672+OsamaAsender@users.noreply.github.com>
2024-06-03 15:27:56 +0300

    Added Assets and Home index page

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoryComponent } from './category/category.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'category',
+    component:CategoryComponent
+  },
+  {
+    path: 'category/create',
+    // component:CategoryComponent
+  },
+  {
+    path: 'category/details/:id',
+    // component:CategoryComponent
+  },  {
+    path: 'category/edit/:id',
+    // component:CategoryComponent
+  },  {
+    path: 'category',
+    // component:CategoryComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
