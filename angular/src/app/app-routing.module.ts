a159ac1a56caa808e21d751fe8ef4a15d0b4ef1e
Osama Asender <103122672+OsamaAsender@users.noreply.github.com>
2024-06-04 10:44:15 +0300

    Removed Assets and the html mock-up

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CategoryComponent } from './category/category.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  {
+    path: 'home',
+    component:HomeComponent
+  },
   {
     path: 'category',
     component:CategoryComponent
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CategoryComponent } from './category/category.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CategoryComponent
+    CategoryComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
