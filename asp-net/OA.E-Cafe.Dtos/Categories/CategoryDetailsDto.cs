5b94a4feeee4739a227dc30971ac08282d275e11
Osama Asender <103122672+OsamaAsender@users.noreply.github.com>
2024-07-31 07:17:09 +0300

    -Added Image Name prop to entity and Dto. In Addition, Created a migration

--- asp-net/OA.E-Cafe.Dtos/Categories/CategoryDetailsDto.cs
+++ asp-net/OA.E-Cafe.Dtos/Categories/CategoryDetailsDto.cs
@@ -7,6 +7,7 @@ namespace OA.E_Cafe.Dtos.Categories
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public string ImageName { get; set; }
         public List<Product> Products { get; set; } = [];
     }
 }
