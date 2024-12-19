e924968ebce384b18259415130297ce7ee1fbdff
Osama Asender <103122672+OsamaAsender@users.noreply.github.com>
2024-12-19 08:12:15 +0300

    -Added Bootstrap -Added Toastr -Added NgxSpinner

--- ECafeClient/src/app/app.component.ts
+++ ECafeClient/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'ECafeClient';
+
+  constructor(
+    private spinner: NgxSpinnerService,
+    private toastr : ToastrService 
+  ){ }
+
+  ngOnInit(): void {
+ this.spinner.show();
+ this.toastr.success('Hello world!', 'Toastr fun!');
+ setTimeout(() => {
+  /** spinner ends after 5 seconds */
+  this.spinner.hide();
+}, 5000);
+
+  }
+  
+  showSuccess() {
+    this.toastr.success('Hello world!', 'Toastr fun!');
+  }
+
+  
+  
 }
--- ECafeClient/src/app/app.module.ts
+++ ECafeClient/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -11,7 +13,9 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxSpinnerModule,
+    ToastrModule.forRoot()
   ],
   providers: [
     provideAnimationsAsync()
